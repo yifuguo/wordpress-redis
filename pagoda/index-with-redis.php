e057fe4500baf2b93e47a17c4a28bc11555fb851
Yifu G. <yifu.guo@bitsyn.com>
2012-11-16 00:49:22 -0500

    Version 0.1 Init

--- pagoda/index-with-redis.php
+++ pagoda/index-with-redis.php
@@ -30,8 +30,8 @@
 
 // change vars here
 $cf = 0;			// set to 1 if you are using cloudflare
-$debug = 1;			// set to 1 if you wish to see execution time and cache actions
-$display_powered_by_redis = 1;  // set to 1 if you want to display a powered by redis message with execution time, see below
+$debug = 0;			// set to 1 if you wish to see execution time and cache actions
+$display_powered_by_redis = 0;  // set to 1 if you want to display a powered by redis message with execution time, see below
 
 $start = microtime();   // start timing page exec
 
